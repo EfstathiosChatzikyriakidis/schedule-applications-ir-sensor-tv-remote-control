76383837dd735e5f1b04f29b6d01789321bc9463
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:39:28 +0300

    Change.

--- development/schedule_applications_ir_sensor_tv_remote_control/eeprom_generic.h
+++ development/schedule_applications_ir_sensor_tv_remote_control/eeprom_generic.h
@@ -1,7 +1,7 @@
 /*
  *  This file defines / implements EEPROM generic template functions.
  *
- *  Copyright (C) 2010  Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010  Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- development/schedule_applications_ir_sensor_tv_remote_control/schedule_applications_ir_sensor_tv_remote_control.ino
+++ development/schedule_applications_ir_sensor_tv_remote_control/schedule_applications_ir_sensor_tv_remote_control.ino
@@ -1,7 +1,7 @@
 /*
  *  Schedule Applications IR Sensor & TV Remote Control.
  *
- *  Copyright (C) 2010  Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010  Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
